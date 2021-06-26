e4a4cf6679517b81e272259c2283e0be86498f6b
Krunalkumar <krunalkumar@mountblue.io>
2021-06-26 15:48:14 +0530

    api made simple

--- grs/complaints/restapi.py
+++ grs/complaints/restapi.py
@@ -1,21 +1,17 @@
 from rest_framework import viewsets
 from .serializer import ComplaintSerializer
 from .models import Complaint
-from rest_framework.decorators import action
-from rest_framework.response import Response
 from rest_framework.authentication import (TokenAuthentication,
-                                           BasicAuthentication)
-from rest_framework.permissions import IsAuthenticated
+                                           BasicAuthentication,
+                                           SessionAuthentication)
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class AssignmentViewSet(viewsets.ModelViewSet):
     serializer_class = ComplaintSerializer
     queryset = Complaint.objects.all()
-    authentication_classes = (BasicAuthentication, TokenAuthentication, )
+    authentication_classes = (BasicAuthentication,
+                              TokenAuthentication,
+                              SessionAuthentication,
+                              )
     permission_classes = (IsAuthenticated, )
-
-    @action(methods=['get'], detail=False)
-    def newest(self, request):
-        newest = self.get_queryset().order_by('due_date').first()
-        serializer = self.get_serializer_class()(newest)
-        return Response(serializer.data)
