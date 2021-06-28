58e00cc72b4a751c9b620cfb6666049c36f57485
Krunalkumar <krunalkumar@mountblue.io>
2021-06-28 15:19:00 +0530

    Token Authentication Added

--- grs/grs/settings.py
+++ grs/grs/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'complaints'
+    'complaints',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
